e0f94e481e3ae3196696b8b84e8dcf5c3cb7c623
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2023-12-12 11:16:55 +0100

    celebrities done workingonemovies

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,33 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const express = require('express');
+const router = express.Router();
+
+const Celebrity = require('../models/Celebrity.model');
+// all your routes here
+
+router.get('/create' ,(req,res) => {
+    res.render('celebrities/new-celebrity')
+})
+
+router.get('/celebrities' ,(req,res) => {
+    Celebrity.find()
+        .then((foundCelebrities) => {
+            console.log('found Celebrities', foundCelebrities)
+            res.render('celebrities/celebrities', {foundCelebrities})
+        })
+        .catch((err) => console.log((err)))
+    
+})
+
+router.post('/create', (req, res) => {
+    Celebrity.create(req.body)
+        .then(() => {
+            res.redirect('celebrities'); // Redirect to the route that displays all celebrities
+        })
+        .catch((err) => {
+            console.log(err);
+            res.redirect('/celebrities/new-celebrity'); // Redirect to the form in case of an error
+        });
+});
+
+module.exports = router;
